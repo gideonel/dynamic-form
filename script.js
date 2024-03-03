8faa8e590304ff3e04415007236138aa9a181a80
gideonel <kryptonianckent99@ymail.com>
2024-03-03 08:50:55 -0800

    first commit

---
+++ script.js
@@ -0,0 +1,17 @@
+function openForm(evt, formName) {
+    var i, tabcontent, tablinks;
+    tabcontent = document.getElementsByClassName("tabcontent");
+    for (i = 0; i < tabcontent.length; i++) {
+        tabcontent[i].style.display = "none";  
+        tabcontent[i].classList.remove("active");
+    }
+    tablinks = document.getElementsByClassName("tablinks");
+    for (i = 0; i < tablinks.length; i++) {
+        tablinks[i].className = tablinks[i].className.replace(" active", "");
+    }
+    document.getElementById(formName).style.display = "block";
+    document.getElementById(formName).classList.add("active");
+    evt.currentTarget.className += " active";
+}
+
+// Get the element with id="defaultOpen" and click on it to open default tab
